b703e23b7003690be9dc76b50380147dcd654cf9
Patrick Moon <patrickmoon@outlook.com>
2020-12-21 16:01:24 -0800

    coding the initial page

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
