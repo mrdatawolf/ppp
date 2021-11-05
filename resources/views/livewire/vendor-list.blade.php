4d63d438ccf50ca67694a5367aba73eb6f27d2d0
Patrick Moon <patrickmoon@outlook.com>
2021-11-05 00:25:18 -0700

    the vendor pick wasn't correctly adjusting to the user actions.  This fixes the breaking behaviour.  There is still a visual issue with the vendor list.

--- resources/views/livewire/vendor-list.blade.php
+++ resources/views/livewire/vendor-list.blade.php
@@ -1,7 +1,7 @@
 <div class="{{ $visiblity }}">
     <ul class="list-reset">
         @foreach ($vendors as $vendor)
-            <li wire:click="$set('vendorClickedName','{{ $vendor['name'] }}')"><p class="p-2 block text-black hover:bg-grey-light cursor-pointer">{{ $vendor['name'] }} @if(!empty($possibleVendorName) && $vendor['name'] === $possibleVendorName) <span class="material-icons-outlined ppp-brown pull-right">check</span>@endif</p></li>
+            <li wire:click="vendorClicked('{{ $vendor['name'] }}')"><p class="p-2 block text-black hover:bg-grey-light cursor-pointer">{{ $vendor['name'] }} @if(!empty($vendorClickedName) && $vendor['name'] === $vendorClickedName) <span class="material-icons-outlined ppp-brown pull-right">check</span>@endif</p></li>
         @endforeach
     </ul>
 </div>
