a315ecfc24a8caaca25efb65126da25d681e2d27
Patrick Moon <patrickmoon@outlook.com>
2021-11-06 23:25:04 -0700

    working thru the issues I've found

--- resources/views/livewire/pick-vendor.blade.php
+++ resources/views/livewire/pick-vendor.blade.php
@@ -5,10 +5,6 @@
         <p class="p-4">Select Vendor: </p>
         <div class="mr-8 ml-4">
             <div class="relative">
-                <button wire:click="flipList" class="bg-ppp-green p-3 rounded text-white shadow-inner w-full">
-                    <label for="vendorString"><span class="float-left">Show vendors</span></label>
-                    <span class="material-icons-outlined ppp-brown">{{ $chevron }}</span>
-                </button>
                 <div class="rounded shadow-md my-2 relative pin-t pin-l">
                     <input wire:model="vendorSearchName" id="vendorString" class="border-2 rounded h-8 w-full">
                     @livewire('vendor-list')
